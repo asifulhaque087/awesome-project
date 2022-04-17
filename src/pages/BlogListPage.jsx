4589e3a318d02ed8c206cd68cb1f30da45628052
asiful087 <asifulhaque087@gmail.com>
2022-04-17 15:14:55 +0600

    create post

--- src/pages/BlogListPage.jsx
+++ src/pages/BlogListPage.jsx
@@ -1,8 +1,10 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const BlogListPage = () => {
   const [state, setState] = useState({ title: "", content: null });
+  const [posts, setPosts] = useState([]);
   const [modal, setModal] = useState(false);
 
   const handleChange = (event) => {
@@ -24,8 +26,19 @@ const BlogListPage = () => {
       return;
     }
 
-    console.log("submitting", state);
+    axios.post("http://localhost:3000/post/", state).then((res) => {
+      fetchPosts();
+    });
   };
+  const fetchPosts = () => {
+    axios.get("http://localhost:3000/post/", state).then((res) => {
+      setPosts(res.data);
+    });
+  };
+
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   return (
     <div>
@@ -46,26 +59,6 @@ const BlogListPage = () => {
                 onClick={() => setModal(false)}
                 className="absolute z-10 top-0 left-0 right-0 bottom-0 bg-black opacity-80"
               />
-              {/* actuall modal */}
-              {/* <div className="fixed z-20 top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] mx-auto p-5 border w-full max-w-sm shadow-lg rounded-md bg-white">
-                <form action="">
-                  <div className="mb-3">
-                    <input
-                      type="text"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="title"
-                    />
-                  </div>
-                  <div>
-                    <textarea
-                      rows={4}
-                      className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                      placeholder="Content..."
-                      defaultValue={""}
-                    />
-                  </div>
-                </form>
-              </div> */}
               <div className="fixed z-20 top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-full max-w-md">
                 <div className="relative p-4 w-full max-w-2xl h-full md:h-auto">
                   {/* Modal content */}
@@ -144,69 +137,71 @@ const BlogListPage = () => {
 
           <div className="flex flex-wrap -m-4">
             {/* 1st */}
-            <div className="p-4 lg:w-1/3">
-              <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
-                <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
-                  CATEGORY
-                </h2>
-                <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
-                  Raclette Blueberry Nextious Level
-                </h1>
-                <p className="leading-relaxed mb-3">
-                  Photo booth fam kinfolk cold-pressed sriracha leggings
-                  jianbing microdosing tousled waistcoat.
-                </p>
-                <Link
-                  to="/blogs/1"
-                  className="text-indigo-500 inline-flex items-center"
-                >
-                  Learn More
-                  <svg
-                    className="w-4 h-4 ml-2"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    strokeWidth={2}
-                    fill="none"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <path d="M5 12h14" />
-                    <path d="M12 5l7 7-7 7" />
-                  </svg>
-                </Link>
-                <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
-                  <span className="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
-                    <svg
-                      className="w-4 h-4 mr-1"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      viewBox="0 0 24 24"
+            {posts &&
+              posts.map((post) => (
+                <div key={post._id} className="p-4 lg:w-1/3">
+                  <div className="h-full bg-gray-100 bg-opacity-75 px-8 pt-16 pb-24 rounded-lg overflow-hidden text-center relative">
+                    <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">
+                      {post.createdAt}
+                    </h2>
+                    <h1 className="title-font sm:text-2xl text-xl font-medium text-gray-900 mb-3">
+                      {post.title}
+                    </h1>
+                    <p className="leading-relaxed mb-3">
+                      {post.content}
+                    </p>
+                    <Link
+                      to="/blogs/1"
+                      className="text-indigo-500 inline-flex items-center"
                     >
-                      <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
-                      <circle cx={12} cy={12} r={3} />
-                    </svg>
-                    1.2K
-                  </span>
-                  <span className="text-gray-400 inline-flex items-center leading-none text-sm">
-                    <svg
-                      className="w-4 h-4 mr-1"
-                      stroke="currentColor"
-                      strokeWidth={2}
-                      fill="none"
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
-                    </svg>
-                    6
-                  </span>
+                      Learn More
+                      <svg
+                        className="w-4 h-4 ml-2"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        strokeWidth={2}
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
+                        <path d="M5 12h14" />
+                        <path d="M12 5l7 7-7 7" />
+                      </svg>
+                    </Link>
+                    <div className="text-center mt-2 leading-none flex justify-center absolute bottom-0 left-0 w-full py-4">
+                      <span className="text-gray-400 mr-3 inline-flex items-center leading-none text-sm pr-3 py-1 border-r-2 border-gray-200">
+                        <svg
+                          className="w-4 h-4 mr-1"
+                          stroke="currentColor"
+                          strokeWidth={2}
+                          fill="none"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z" />
+                          <circle cx={12} cy={12} r={3} />
+                        </svg>
+                        1.2K
+                      </span>
+                      <span className="text-gray-400 inline-flex items-center leading-none text-sm">
+                        <svg
+                          className="w-4 h-4 mr-1"
+                          stroke="currentColor"
+                          strokeWidth={2}
+                          fill="none"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          viewBox="0 0 24 24"
+                        >
+                          <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z" />
+                        </svg>
+                        6
+                      </span>
+                    </div>
+                  </div>
                 </div>
-              </div>
-            </div>
+              ))}
           </div>
         </div>
       </section>
